6019a487b0232fa2acc49dbe5b1a588ab4e2f3a5
CodeCoaster <thecodecoaster@gmail.com>
2017-06-08 01:26:12 -0400

    work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,5 +3,27 @@ class WorksController < ApplicationController
     @work_items = Work.all
   end
 
+#render the work form
+  def new 
+    @work_item = Work.new
+  end
+ 
+#create the work when form submitted
+  def create
+    @work = Work.new(params.require(:work).permit(:title, :subtitle, :body))
+    
+    respond_to do |format|
+      if @work.save
+        format.html { redirect_to @work, notice: 'Work item was successfully created!'}
+        format.json { render :show, status: :created, location: @work }
+      else
+        format.html { render :new }
+        format.json { render json: @work.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
+  def show
+  end
+  
 end
