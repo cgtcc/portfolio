1788f1fe6fb0ec7ea85ba3991112c75ccdc26ec5
CodeCoaster <thecodecoaster@gmail.com>
2017-06-09 02:57:22 -0400

    draft / published status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,8 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.published + Blog.draft
+
   end
 
   # GET /blogs/1
