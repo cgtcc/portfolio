8cae05296e159619697650ca8ad190b3ee95511d
CodeCoaster <thecodecoaster@gmail.com>
2017-06-19 11:52:42 -0400

    placeholder default image url

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -2,7 +2,7 @@ module Placeholder
     extend ActiveSupport::Concern
 
     def self.image_generator(height:, width:)
-        "http://placeholder.it/#{height}x#{width}"
+        "http://placeholder.com/#{height}x#{width}"
     end
     
 end
