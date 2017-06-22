f1ac5608610f4ecb05e96abd6d2f937664467f8b
CodeCoaster <thecodecoaster@gmail.com>
2017-06-22 14:30:54 -0400

    user management w/ devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,12 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = 
+  
   config.action_mailer.perform_caching = false
+
+  #In production, :host should be set to the actual host of the application
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
