79a171d569923599cdde2d6a6f00b2838bcdc6fe
CodeCoaster <thecodecoaster@gmail.com>
2017-06-22 15:16:22 -0400

    user management migration

--- db/migrate/20170622184958_devise_create_users.rb
+++ db/migrate/20170622184958_devise_create_users.rb
@@ -9,6 +9,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
+      ## Custom fields
+      t.string :name
+
       ## Rememberable
       t.datetime :remember_created_at
 
@@ -20,10 +23,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+       t.string   :confirmation_token
+       t.datetime :confirmed_at
+       t.datetime :confirmation_sent_at
+       t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
