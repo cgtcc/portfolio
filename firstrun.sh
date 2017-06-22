ee93e1a8eed491b0f67821e0de660f90b7636a56
CodeCoaster <thecodecoaster@gmail.com>
2017-06-22 14:16:26 -0400

    run this file at first, to install sample data

--- firstrun.sh
+++ firstrun.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-rake db:drop db:create db:migrate db:setup
+echo 'drop the database'
+rake db:drop 
+echo 'create the database'
+rake db:create 
+echo 'create the database tables'
+rake db:migrate 
+echo 'add sample data to the database'
+rake db:setup
+echo 'generate devise configuration for user management'
 rails generate devise:install
+echo "let's start the server, on port 3000"
 rails s
 
