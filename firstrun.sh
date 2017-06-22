b06871ab204eeee93e3ea920efc02e7bd1adb634
CodeCoaster <thecodecoaster@gmail.com>
2017-06-22 14:48:32 -0400

    updated firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -7,8 +7,6 @@ echo 'create the database tables'
 rake db:migrate 
 echo 'add sample data to the database'
 rake db:setup
-echo 'generate devise configuration for user management'
-rails generate devise:install
 echo "let's start the server, on port 3000"
 rails s
 
