6c8d2992407e7f284f8de0d875a297d3e11eca06
CodeCoaster <thecodecoaster@gmail.com>
2017-06-23 11:28:51 -0400

    before filter to before action

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,9 +1,9 @@
 module DeviseWhitelist
     extend ActiveSupport::Concern
 #this is a concern module for custom user and admin parameters in devise (see schema.rb file)
-
-    included do
-        before_filter :custom_form_params, if: :devise_controller?
+#see http://www.rubydoc.info/github/plataformatec/devise/Devise/Controllers/Helpers
+included do
+            before_action :custom_form_params, if: :devise_controller?
     end
 
     def custom_form_params
