8309cd1fc70f1eeb30a5d677f3ee42db00dac31a
CodeCoaster <thecodecoaster@gmail.com>
2017-06-23 11:16:37 -0400

    module concern for user custom fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #following include add custom form parameters, and refer to controller concern file devise_whitelist.rb
+  include DeviseWhitelist
 end
