d413d03d52ae8f10cf41a182a57030a1c117f056
CodeCoaster <thecodecoaster@gmail.com>
2017-06-23 12:16:40 -0400

    integrated virtual attributes to extract first and last name data from a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+
+
+         def first_name
+           self.name.split.first
+         end
+
+         def last_name
+           self.name.split.last
+         end
+
 end
