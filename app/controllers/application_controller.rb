6b466e1cd787a49d5da4addfeba7f86b9c9d43aa
CodeCoaster <thecodecoaster@gmail.com>
2017-06-25 09:18:46 -0400

    define current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,10 @@ class ApplicationController < ActionController::Base
 
 #overriding the current_user method
 #we want current user to be available even if current_user is not logged in
+#example in PRY : 
+# OpenStruct.new(name: "name", first_name: "first_name", last_name:"last name", email: "my@email.com")
   def current_user 
-    super #will never return NIL
+    super ||  #will never return NIL, create a fake user
   end
 
 
