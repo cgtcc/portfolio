ca82347ab219a7eaba3dcd68d9e5364f3168b1da
CodeCoaster <thecodecoaster@gmail.com>
2017-07-21 11:40:06 -0400

    application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,20 @@
 module ApplicationHelper
-    def sample_helper
-        "<p>My helper</p>".html_safe
+    def login_helper
+         if current_user.is_a?(User) 
+            link_to "logout", destroy_user_session_path, method: :delete  
+            else 
+            (link_to "login", new_user_session_path, method: :get  ) +
+            "<br />".html_safe +
+            (link_to "register", new_user_registration_path, method: :get)  
+         end
     end
+
+    def source_helper
+      if session[:source]
+        greeting = "Thanks for visiting me from  #{session[:source]}"
+        content_tag(:p, greeting, class: "source-greeting")
+      end
+    end
+
     
 end
