43265b44a22f21214c7db092cc0ec033923dff1e
CodeCoaster <thecodecoaster@gmail.com>
2017-08-08 14:42:02 -0400

    refactored guest u_user method

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -8,19 +8,26 @@ end
 #we want current user to be available even if current_user is not logged in
 #example for testing w/ PRY : 
 # OpenStruct.new(name: "name", first_name: "first_name", last_name:"last name", email: "my@email.com")
-def current_user 
-        #will never return NIL, create a fake user
-        #will return the super value (logged in user) OR the guest infos.
-    super ||  guest_user
-end
+      def current_user 
+              #will never return NIL, create a fake user
+              #will return the super value (logged in user) OR the guest infos.
+          super ||  guest_user
+      end
 
-def guest_user
-    OpenStruct.new(name: "Guest", 
-                                      first_name: "Guest", 
-                                      last_name: "Guest", 
-                                      email: "guest@unknow"
-                                      )
-end
+      def guest_user
+       # OpenStruct.new(name: "Guest", 
+       #                                   first_name: "Guest", 
+       #                                   last_name: "Guest", 
+       #                                   email: "guest@unknow"
+       #                                   )
+
+       guest = GuestUser.new
+       guest.name = "Guest User"
+       guest.first_name = "Guest"
+       guest.last_name = "User"
+       guest.email = "guest@example.com"
+       guest
+      end
 
 
     
---
+++ app/controllers/guest_user.rb
@@ -0,0 +1,8 @@
+class GuessUser < User
+
+  attr_accessor :name, :first_name, :last_name, :email
+
+  
+
+
+end
